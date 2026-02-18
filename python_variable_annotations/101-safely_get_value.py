c7ffa90ff9eef11a24a2146e5a9db4791feaaaac
Yara Alrasheed <11982@holbertonstudents.com>
2026-02-18 21:39:46 +0300

    Update 101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+"""Module for safely_get_value function"""
 
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+  dct: Mapping,
+  key: Any,
+  default: Union[T, None] = None
+) -> Union[Any, T]
+"""Return the value for key in dct if present, otherwise default"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default

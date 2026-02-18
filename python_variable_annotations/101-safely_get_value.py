bac724ba2a622a63993b9345af2547201cffa13e
Yara Alrasheed <11982@holbertonstudents.com>
2026-02-18 21:40:39 +0300

    Update 101-safely_get_value.py

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,11 +7,11 @@ T = TypeVar('T')
 
 
 def safely_get_value(
-  dct: Mapping,
-  key: Any,
-  default: Union[T, None] = None
-) -> Union[Any, T]
-"""Return the value for key in dct if present, otherwise default"""
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Return the value for key in dct if present, otherwise default"""
     if key in dct:
         return dct[key]
     else:

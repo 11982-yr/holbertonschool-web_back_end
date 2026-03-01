bf075143577871c3bfb5da01a8c0f7b906aba8d9
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-01 20:19:25 +0300

    Fix formatting and indentation in index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,8 +6,8 @@ Simple helper function for pagination
 
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Return a tuple of size two containing a start index and an end index 
-    corresponding to the range of indexes to return 
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return
     in a list for those particular pagination parameters.
 
     Page numbers are 1-indexed.

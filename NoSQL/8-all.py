1277878b3407aa33b057239f6e632ec8413b825e
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-02 20:29:27 +0300

    Implement list_all function for MongoDB documents
    Add a function to list all documents in a MongoDB collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""List documents"""
+import pymongo
 
+
+def list_all(mongo_collection) -> list:
+    """Lists all documents in a collection
+       Args:
+           mongo_collection: collection of objects
+
+        Return:
+            List with documents, otherwise []
+    """
+    documents: list = []
+
+    for document in mongo_collection.find():
+        documents.append(document)
+
+    return documents

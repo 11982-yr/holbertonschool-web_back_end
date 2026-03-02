177b588931d90b08fec0b3915e69cbd8ca176183
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-02 20:35:07 +0300

    Implement insert_school function for MongoDB
    Added a function to insert a school document into a MongoDB collection.

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+""" Insert document """
+import pymongo
 
+
+def insert_school(mongo_collection, **kwargs):
+    """ Insert a school with features
+
+    Args:
+        mongo_collection: collection to pass
+        kwargs: dictionary with elements to put
+    
+    Return:
+        id of the new element
+    """
+    new_school = mongo_collection.insert_one(kwargs)
+
+    return (new_school.inserted_id)

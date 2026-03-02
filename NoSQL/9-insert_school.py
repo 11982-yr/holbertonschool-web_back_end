b96574d610dfefa1ebfee65e7076be3c85595e14
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-02 20:35:59 +0300

    Update docstring formatting in insert_school function

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,12 +6,12 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """ Insert a school with features
 
-    Args:
-        mongo_collection: collection to pass
-        kwargs: dictionary with elements to put
+        Args:
+            mongo_collection: collection to pass
+            kwargs: dictionary with elements to put
     
-    Return:
-        id of the new element
+        Return:
+            id of the new element
     """
     new_school = mongo_collection.insert_one(kwargs)
 

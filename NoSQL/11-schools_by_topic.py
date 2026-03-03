e634bb7f5195b73cd03363ba02d5f6aec9dc0869
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-03 20:57:28 +0300

    Implement schools_by_topic function
    Fix the function to append schools to the list correctly.

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
+""" Schools by topic """
+import pymongo
+from typing import List
 
+
+def schools_by_topic(mongo_collection, topic):
+    """ Returns the list of school having a specific topic
+
+        Args:
+            mongo_collection: collection to pass
+            topic: topic searched
+    
+        Return:
+            List of schools by topic
+    """
+    query: dict = {'topics': topic}
+    schools: list = []
+
+    for school in mongo_collection.find(query):
+        schools = append(school)
+
+    return schools

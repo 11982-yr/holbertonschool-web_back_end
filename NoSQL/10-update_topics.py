e154fbe41ab66ed17fd7b8cf4c5ab96810b545fa
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-03 20:26:56 +0300

    Implement update_topics function for school topics
    Add function to update topics of a school document.

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+""" Update document """
+import pymongo
 
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name
+
+        Args:
+            mongo_collection: collection to pass
+            name: school name to update
+            topics: list of topics
+    
+        Return:
+            id of the new element
+    """
+    update_topics = mongo_collection.insert_one(kwargs)
+
+    return (new_school.inserted_id)

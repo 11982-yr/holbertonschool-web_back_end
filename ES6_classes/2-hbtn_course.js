05165ac2113d465243c5b1cde5d67b16bfdc66c0
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-12 20:26:54 +0300

    Update 2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -28,5 +28,19 @@ export default class HolbertonCourse {
   }
 
   get students() {
-    if (typeof currStudents == 'object') {
-      for (const Students
+    return this._students;
+  }
+
+  set students(currStudents) {
+    if (typeof currStudents === 'object') {
+      for (const student in currStudents) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = currStudents;
+  }
+}

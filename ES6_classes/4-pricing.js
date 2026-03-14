13a3d2a2e0a455b7f6c5f6efb75fcba9eb42d53f
Yara Alrasheed <11982@holbertonstudents.com>
2026-03-14 22:50:38 +0300

    Update 4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -7,16 +7,45 @@ export default class Pricing {
     this.currency = currency;
   }
 
-displayFullPrice() {
-  const code = (this.currency_code);
-  const name = (this.currency._name);
-  const money = `${this.amount} ${name} (${code})`;
+  displayFullPrice() {
+    const code = (this.currency._code);
+    const name = (this.currency._name);
+    const money = `${this.amount} ${name} (${code})`;
 
-  return money;
-}
+    return money;
+  }
 
   static convertPrice(amount = 0, conversionRate = 0) {
     if (typeof amount !== 'number') {
       throw new TypeError('amount must be a string');
     }
-    
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a string');
+    }
+
+    return (amount * conversionRate);
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+}

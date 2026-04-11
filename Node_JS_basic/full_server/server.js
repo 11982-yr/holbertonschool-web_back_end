55a72a68106ffba39b7de25b87849372e5658f2f
Yara Alrasheed <11982@holbertonstudents.com>
2026-04-11 14:46:27 +0300

    Update server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import router from './routes';
+import router from './routes/index.js';
 
 const app = express();
 
-app.use('/', router);
+app.use(router);
 
 app.listen(1245);
 

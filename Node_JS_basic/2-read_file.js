05f180aba481a58854e20bdfbc2782fb46891488
Yara Alrasheed <11982@holbertonstudents.com>
2026-04-11 14:29:50 +0300

    Create 2-read_file.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+  const students = lines.slice(1); // remove header
+
+  console.log(`Number of students: ${students.length}`);
+
+  const fields = {};
+
+  students.forEach((line) => {
+    const parts = line.split(',');
+    const firstname = parts[0];
+    const field = parts[3];
+
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(firstname);
+  });
+
+  Object.keys(fields).forEach((field) => {
+    const list = fields[field];
+    console.log(
+      `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`
+    );
+  });
+}
+
+module.exports = countStudents;

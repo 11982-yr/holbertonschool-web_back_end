04eeb454b109983bcbd96acc2be2ee7dbe08b88a
Yara Alrasheed <11982@holbertonstudents.com>
2026-04-11 14:51:19 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,29 +1,34 @@
-import fs from 'fs';
+const fs = require('fs');
 
-const readDatabase = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
-
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
-    const fields = {};
-
-    students.forEach((line) => {
-      const student = line.split(',');
-      const firstname = student[0];
-      const field = student[3];
-
-      if (!fields[field]) {
-        fields[field] = [];
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
-      fields[field].push(firstname);
-    });
+      const content = data.toString().split('\n');
 
-    resolve(fields);
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
+    });
   });
-});
+}
 
 export default readDatabase;
